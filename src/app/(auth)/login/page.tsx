5b2cc597aa403aefc66eea29aa64f3133e113ca8
IsuruKavi <isurukavindalakshan863@gmail.com>
2025-09-17 12:52:57 +0530

    add signup

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -5,51 +5,12 @@ import { Input } from '@/components/ui/input';
 
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+import AuthForm from '@/components/auth/AuthForm';
 function SignIn() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log(email, password);
-  };
+
 
   return (
-    <div className="">
-      <p className="mb-8 text-4xl font-bold text-black">Login</p>
-      <div className="grid w-full max-w-sm items-center gap-3">
-        <form onSubmit={handleSubmit}>
-          <Input
-            type="email"
-            id="email"
-            placeholder="Email"
-            className="mb-4 h-12 w-90"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Input
-            type="email"
-            id="email"
-            placeholder="password"
-            className="mb-4 h-12 w-90"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Button
-            variant="default"
-            type="submit"
-            className="h-12 w-full rounded-full bg-rose-400 text-white hover:bg-red-300 hover:text-white"
-          >
-            Log in
-          </Button>
-        </form>
-        <div className="flex flex-row justify-center">
-          <p className="mr-2 text-black">Don't have account ?</p>
-          <Link href={'/auth/signup'}>
-            <p className="text-rose-400">create account</p>{' '}
-          </Link>
-        </div>
-      </div>
-    </div>
+    <AuthForm type="login"/>
   );
 }
 
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,65 +1,13 @@
-'use client';
+"use client";
 
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
-import Link from 'next/link';
-import React, { useState } from 'react';
+import AuthForm from "@/components/auth/AuthForm";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import Link from "next/link";
+import React, { useState } from "react";
 
 function SignUp() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const handleSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log(email, password);
-  };
-
-  return (
-    <div className="">
-      <p className="mb-8 text-4xl font-bold text-black">Sign Up</p>
-      <div className="grid w-full max-w-sm items-center gap-3">
-        <form onSubmit={handleSubmit}>
-          <Input
-            type="email"
-            id="email"
-            placeholder="Email"
-            className="mb-4 h-12 w-90"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          <Input
-            type="password"
-            id="email"
-            placeholder="password"
-            className="mb-4 h-12 w-90"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          <Input
-            type="password"
-            id="email"
-            placeholder="confirm password"
-            className="mb-4 h-12 w-90"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-          <Button
-            variant="default"
-            type="submit"
-            className="h-12 w-full rounded-full bg-rose-400 text-white hover:bg-red-300 hover:text-white"
-          >
-            Signup
-          </Button>
-        </form>
-        <div className="flex flex-row justify-center">
-          <p className="mr-2 text-black font-medium">Already have an account ?</p>
-          <Link href={'/auth/login'}>
-            <p className="text-rose-400 font-medium ">Log in</p>{' '}
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
+  return <AuthForm type="signup" />;
 }
 
 export default SignUp;
